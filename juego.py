e0d96c1195d2470b68574a09960d0c6216d29b7b
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-03-16 20:04:24 -0600

    feat: count time to execute each model

--- juego.py
+++ juego.py
@@ -1,3 +1,4 @@
+import time
 from board_and_rules import Connect4, minimax
 
 
@@ -9,6 +10,9 @@ def play_game():
         "Elige el modo de juego: 1 (Humano vs IA) o 2 (IA vs IA - Sin poda vs Con poda): "
     )
     human_players = mode == "1"
+
+    time_ai_1 = 0
+    time_ai_2 = 0
 
     while True:
         game.print_board()
@@ -32,15 +36,25 @@ def play_game():
             game.drop_piece(column, player)
         else:
             use_alpha_beta = player == 2
+            start_time = time.time()
             column, _ = minimax(
                 game, 4, -float("inf"), float("inf"), True, player, use_alpha_beta
             )
+            timing = time.time() - start_time
+
+            if player == 1:
+                time_ai_1 += timing
+            else:
+                time_ai_2 += timing
+
             print(
                 "----------------------------------------player: ",
                 player,
                 "esta usando alpha_beta?: ",
                 use_alpha_beta,
-                "----------------------------------------",
+                "tiempo tomado: ",
+                round(timing, 4),
+                "s ----------------------------------------",
             )
             game.drop_piece(column, player)
             print(
@@ -58,6 +72,10 @@ def play_game():
             break
 
         turn += 1
+
+    print("\nTiempo total de ejecución:")
+    print(f"AI 1 (sin poda): {round(time_ai_1, 4)} s")
+    print(f"AI 2 (con poda): {round(time_ai_2, 4)} s")
 
 
 if __name__ == "__main__":
