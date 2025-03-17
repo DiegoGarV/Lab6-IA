cebcfe1952658e4aa44e6845d86ed15d33b64b39
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2025-03-16 20:10:10 -0600

    feat: add replay option at the end of every game

--- juego.py
+++ juego.py
@@ -3,79 +3,86 @@ from board_and_rules import Connect4, minimax
 
 
 def play_game():
-    game = Connect4()
-    turn = 1
-
-    mode = input(
-        "Elige el modo de juego: 1 (Humano vs IA) o 2 (IA vs IA - Sin poda vs Con poda): "
-    )
-    human_players = mode == "1"
-
-    time_ai_1 = 0
-    time_ai_2 = 0
-
     while True:
-        game.print_board()
-        player = 1 if turn % 2 != 0 else 2
+        game = Connect4()
+        turn = 1
 
-        if human_players and player == 1:
-            try:
-                column = int(input(f"Jugador {player}, elige una columna (1-7): ")) - 1
-            except ValueError:
-                print("Entrada inválida. Intenta nuevamente.")
-                continue
+        mode = input(
+            "Elige el modo de juego: \n1 (Humano vs IA) \no \n2 (IA vs IA - Sin poda vs Con poda) \n"
+        )
+        human_players = mode == "1"
 
-            if column not in range(7):
-                print("Columna fuera de rango. Intenta nuevamente.")
-                continue
+        timer_1 = 0
+        timer_2 = 0
 
-            if not game.is_valid_location(column):
-                print("Columna llena. Intenta nuevamente.")
-                continue
+        while True:
+            game.print_board()
+            player = 1 if turn % 2 != 0 else 2
 
-            game.drop_piece(column, player)
-        else:
-            use_alpha_beta = player == 2
-            start_time = time.time()
-            column, _ = minimax(
-                game, 4, -float("inf"), float("inf"), True, player, use_alpha_beta
-            )
-            timing = time.time() - start_time
+            if human_players and player == 1:
+                try:
+                    column = (
+                        int(input(f"Jugador {player}, elige una columna (1-7): ")) - 1
+                    )
+                except ValueError:
+                    print("Entrada inválida. Intenta nuevamente.")
+                    continue
 
-            if player == 1:
-                time_ai_1 += timing
+                if column not in range(7):
+                    print("Columna fuera de rango. Intenta nuevamente.")
+                    continue
+
+                if not game.is_valid_location(column):
+                    print("Columna llena. Intenta nuevamente.")
+                    continue
+
+                game.drop_piece(column, player)
             else:
-                time_ai_2 += timing
+                use_alpha_beta = player == 2
+                start_time = time.time()
+                column, _ = minimax(
+                    game, 4, -float("inf"), float("inf"), True, player, use_alpha_beta
+                )
+                elapsed_time = time.time() - start_time
 
-            print(
-                "----------------------------------------player: ",
-                player,
-                "esta usando alpha_beta?: ",
-                use_alpha_beta,
-                "tiempo tomado: ",
-                round(timing, 4),
-                "s ----------------------------------------",
-            )
-            game.drop_piece(column, player)
-            print(
-                f"Jugador {player} (AI{' con poda' if use_alpha_beta else ' sin poda'}) elige la columna: {column+1}"
-            )
+                if player == 1:
+                    timer_1 += elapsed_time
+                else:
+                    timer_2 += elapsed_time
 
-        if game.check_winner(player):
-            game.print_board()
-            print(f"¡Jugador {player} ha ganado!")
+                print(
+                    "------------------------------------player: ",
+                    player,
+                    "esta usando alpha_beta?: ",
+                    use_alpha_beta,
+                    "tiempo tomado: ",
+                    round(elapsed_time, 4),
+                    "s ------------------------------------",
+                )
+                game.drop_piece(column, player)
+                print(
+                    f"Jugador {player} (AI{' con poda' if use_alpha_beta else ' sin poda'}) elige la columna: {column+1}"
+                )
+
+            if game.check_winner(player):
+                game.print_board()
+                print(f"¡Jugador {player} ha ganado!")
+                break
+
+            if len(game.get_valid_columns()) == 0:
+                game.print_board()
+                print("¡Empate!")
+                break
+
+            turn += 1
+
+        print("\nTiempo total:")
+        print(f"AI sin poda: {round(timer_1, 4)} s")
+        print(f"AI con poda: {round(timer_2, 4)} s")
+
+        replay = input("¿Quieres jugar de nuevo? (s/n): ").lower()
+        if replay != "s":
             break
-
-        if len(game.get_valid_columns()) == 0:
-            game.print_board()
-            print("¡Empate!")
-            break
-
-        turn += 1
-
-    print("\nTiempo total de ejecución:")
-    print(f"AI 1 (sin poda): {round(time_ai_1, 4)} s")
-    print(f"AI 2 (con poda): {round(time_ai_2, 4)} s")
 
 
 if __name__ == "__main__":
